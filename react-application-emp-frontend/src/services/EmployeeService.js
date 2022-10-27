d3f85cc561101c6b8ba08b0e368582d24a8d3a22
sandipchavan <sandipchavan1994@gmail.com>
2022-10-27 11:33:59 +0530

    base url

--- react-application-emp-frontend/src/services/EmployeeService.js
+++ react-application-emp-frontend/src/services/EmployeeService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const EMPLOYEE_API_BASE_URL = "http://3.86.222.211:8000/api/v1/employees"
+const EMPLOYEE_API_BASE_URL = "http://localhost:8000/api/v1/employees"
 class EmployeeService {
      getEmployees(){
          return axios.get(EMPLOYEE_API_BASE_URL);
