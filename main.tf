106fbccfc42526c174d8cf509ab2cb6686792e5c
sid1909 <83674531+sid1909@users.noreply.github.com>
2022-10-28 12:02:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_file="/home/ubuntu/.aws"
+  shared_credentials_file="/home/ubuntu/.aws/credentials"
 }
 
 data "aws_ami" "ubuntu" {
