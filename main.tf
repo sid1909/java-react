7619c8d2161044cbaa4bd3bcd01f7385d66423c9
sid1909 <83674531+sid1909@users.noreply.github.com>
2022-10-28 09:46:09 +0530

    public ip address

--- main.tf
+++ main.tf
@@ -22,14 +22,14 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.ec2_instance.id
-  allocation_id = aws_eip.example.id
+data "aws_eip" "my_instance_eip" {
+  public_ip = "3.220.181.223"
 }
 
-resource "aws_eip" "example" {
-  vpc = true
-} 
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.ec2_instance.id
+  allocation_id = data.aws_eip.my_instance_eip.id
+}
 
 resource "aws_instance" "ec2_instance" {
   ami                    = data.aws_ami.ubuntu.id
